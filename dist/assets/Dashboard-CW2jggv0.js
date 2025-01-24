205b17ed6b63906ff4418490272ac3b937383d62
Farhan22798 <mohdfarhan227@gmail.com>
2025-01-24 16:04:20 +0530

    rider detaild for restaurant socket

---
+++ dist/assets/Dashboard-CW2jggv0.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Q6tj9H2x.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
--- dist/assets/Dashboard-d0YyPwca.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DJYk6el3.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
