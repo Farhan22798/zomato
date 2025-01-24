d3dbd2093297ce03190c5cfd44e9008b15458e14
Farhan22798 <mohdfarhan227@gmail.com>
2025-01-24 15:45:42 +0530

    socket import wrong

--- dist/assets/Dashboard-D8lU3VrX.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-4Uqc8lKu.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
---
+++ dist/assets/Dashboard-d0YyPwca.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DJYk6el3.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
