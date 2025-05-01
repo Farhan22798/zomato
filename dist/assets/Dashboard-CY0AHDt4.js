43b660b73c33f85d90a8dc995fcceb8ae69a1753
Farhan22798 <mohdfarhan227@gmail.com>
2025-05-01 18:18:48 +0530

    fixed links for apps

--- dist/assets/Dashboard-CY0AHDt4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DyCdYj7e.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
---
+++ dist/assets/Dashboard-DJn_Exf1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D_IVbXsZ.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
