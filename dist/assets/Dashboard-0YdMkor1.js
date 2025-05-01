bc28157a085d193828a47620bd7de05c41558548
Farhan22798 <mohdfarhan227@gmail.com>
2025-05-01 18:09:38 +0530

    fixed app links downloadable

--- dist/assets/Dashboard-0YdMkor1.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BmimkFc6.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
---
+++ dist/assets/Dashboard-CY0AHDt4.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DyCdYj7e.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
